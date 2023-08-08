12adc4a92965def3c256a9c940257d04edf3a444
Marvin Beym <mBeym@databay.de>
2023-08-08 08:40:14 +0200

    Update plugin files

--- plugin.php
+++ plugin.php
@@ -5,8 +5,8 @@ $id = "tmsq";
 
 $version = "1.0.0";
 
-$ilias_min_version = "5.4";
-$ilias_max_version = "7.999";
+$ilias_min_version = "8.0";
+$ilias_max_version = "8.999";
 
 $responsible = "Fabian Sesterhenn";
 $responsible_mail = "fabian.sesterhenn@th-koeln.de";
