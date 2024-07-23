794645b485f7506809822e92b3d9e3c0b8245b22
Marvin Beym <mBeym@databay.de>
2024-07-23 10:05:02 +0200

    Update project files

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,24 +1,35 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . "/vendor/autoload.php";
+
+$dirs = array_filter([
+    __DIR__ . "/src",
+    __DIR__ . "/classes"
+], static function (string $dir): bool {
+    return is_dir($dir);
+});
 
 $finder = PhpCsFixer\Finder::create()
-    ->exclude(array(
-        __DIR__ . "/vendor",
-    ))
-    ->in([
-        __DIR__ . "/src",
-        __DIR__ . "/classes",
-
-    ]);
+    ->exclude([__DIR__ . "/vendor"])
+    ->in($dirs)
+    ->name("*.php");
 
 return (new PhpCsFixer\Config())
     ->setUsingCache(false)
     ->setFinder($finder)
     ->setRules([
-        '@PSR12' => true,
-        'cast_spaces' => true,
-        'strict_param' => false,
-        'concat_space' => ['spacing' => 'one'],
-        'function_typehint_space' => true
+        "@PSR12" => true,
+        "strict_param" => false,
+        "cast_spaces" => true,
+        "concat_space" => ["spacing" => "one"],
+        "unary_operator_spaces" => true,
+        "type_declaration_spaces" => true,
+        "binary_operator_spaces" => true,
+        "array_syntax" => ["syntax" => "short"],
+        "no_superfluous_phpdoc_tags" => [
+            "allow_mixed" => true,
+            "remove_inheritdoc" => true,
+        ],
+        "no_empty_phpdoc" => true,
+        "no_unused_imports" => true
     ]);
